f4c64817f4f96cb805625bfcae6882fa8b438816
Peace Chen <peace@temperal.co>
2024-02-15 01:58:32 -0600

    1.0.4
    Convert to path params for CDN caching

--- src/index.tsx
+++ src/index.tsx
@@ -84,32 +84,32 @@ export function generateUrl(args: PhotoSynthProps): GenerateUrlResult {
     return { error: "Please provide a valid image URL" };
   }
 
-  let url = `${REACT_APP_PHOTOSYNTH_URL}?u=${sourceUrl}&k=${key}`;
-  if (validateValue({ max: 5000, min: 1, value: width })) { url += `&w=${width}` }
-  if (validateValue({ max: 5000, min: 1, value: height })) { url += `&h=${height}` }
-  if (validateValue({ max: 100, min: 0, value: adaptiveHistogram })) { url += `&ah=${adaptiveHistogram}` }
-  if (validateValue({ max: 20, min: 0.2, type: "float", value: blur })) { url += `&b=${blur}` }
-  if (validateValue({ max: 20, min: 0, type: "float", value: brightness })) { url += `&br=${brightness}` }
+  let url = `${REACT_APP_PHOTOSYNTH_URL}/u=${sourceUrl},k=${key}`;
+  if (validateValue({ max: 5000, min: 1, value: width })) { url += `,w=${width}` }
+  if (validateValue({ max: 5000, min: 1, value: height })) { url += `,h=${height}` }
+  if (validateValue({ max: 100, min: 0, value: adaptiveHistogram })) { url += `,ah=${adaptiveHistogram}` }
+  if (validateValue({ max: 20, min: 0.2, type: "float", value: blur })) { url += `,b=${blur}` }
+  if (validateValue({ max: 20, min: 0, type: "float", value: brightness })) { url += `,br=${brightness}` }
   if (validateValue({ max: 99, min: 1, value: cropLeftPercent }) ||
       validateValue({ max: 99, min: 1, value: cropTopPercent }) ||
       validateValue({ max: 99, min: 1, value: cropRightPercent }) ||
       validateValue({ max: 99, min: 1, value: cropBottomPercent }))
   {
-    url += `&c=${cropLeftPercent ?? 0},${cropTopPercent ?? 0},${cropRightPercent ?? 0},${cropBottomPercent ?? 0}`;
+    url += `,c=${cropLeftPercent ?? 0}_${cropTopPercent ?? 0}_${cropRightPercent ?? 0}_${cropBottomPercent ?? 0}`;
   }
-  if (validateValue({ max: 3, min: 1, type: "float", value: gamma })) { url += `&ga=${gamma}` }
-  if (validateValue({ max: 180, min: 1, type: "float", value: hue })) { url += `&hu=${hue}` }
-  if (validateValue({ max: 200, min: 0, type: "float", value: lightness })) { url += `&l=${lightness}` }
+  if (validateValue({ max: 3, min: 1, type: "float", value: gamma })) { url += `,ga=${gamma}` }
+  if (validateValue({ max: 180, min: 1, type: "float", value: hue })) { url += `,hu=${hue}` }
+  if (validateValue({ max: 200, min: 0, type: "float", value: lightness })) { url += `,l=${lightness}` }
   if (validateValue({ max: 99, min: 1, value: normalizeLower }) &&
       validateValue({ max: 99, min: 1, value: normalizeUpper }) &&
       (normalizeUpper ?? 0) > (normalizeLower ?? 0))
   {
-    url += `&n=${normalizeLower},${normalizeUpper}`
+    url += `,n=${normalizeLower}_${normalizeUpper}`
   }
-  if (validateValue({ max: 20, min: 0, type: "float", value: saturation })) { url += `&s=${saturation}` }
-  if (validateValue({ max: 10, min: 0.1, type: "float", value: sharpen })) { url += `&sh=${sharpen}` }
-  if (greyscale) { url += `&gr=${greyscale}` }
-  if (format) { url += `&o=${format}` }
+  if (validateValue({ max: 20, min: 0, type: "float", value: saturation })) { url += `,s=${saturation}` }
+  if (validateValue({ max: 10, min: 0.1, type: "float", value: sharpen })) { url += `,sh=${sharpen}` }
+  if (greyscale) { url += `,gr=${greyscale}` }
+  if (format) { url += `,o=${format}` }
   url = processCacheBust(url, cacheBust)
 
   return { url };
@@ -117,13 +117,12 @@ export function generateUrl(args: PhotoSynthProps): GenerateUrlResult {
 
 //---------------------------------------------------------
 function processCacheBust(url: string, cacheBust: boolean | string | undefined) {
-  let delimiter = url.includes("?") ? "&" : "?";
   if (cacheBust === true) {
     // Appending the query param none=${cacheBustVal} busts the browser cache
-    return `${url}${delimiter}none=${Date.now()}`;
+    return `${url}?none=${Date.now()}`;
   }
   else if (cacheBust) { // string is truthy
-    return `${url}${delimiter}none=${cacheBust}`;
+    return `${url}?none=${cacheBust}`;
   }
   return url;
 }
